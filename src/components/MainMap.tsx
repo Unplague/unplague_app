2e90aca1eb3b8e43caf9f7dfa9ef9d5675346f3d
weinrero <roland.weinreich@gmail.com>
2020-03-21 13:55:44 +0100

    add country features to map

--- src/components/MainMap.tsx
+++ src/components/MainMap.tsx
@@ -1,32 +1,42 @@
 import React from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, Marker, Popup, TileLayer, GeoJSON } from 'react-leaflet';
 import { LatLngTuple } from 'leaflet';
 import 'leaflet-css';
+import countries from '../data/countries.json';
 
 class MainMap extends React.Component {
 
     state = {
         lat: 0,
         lng: 0,
-        zoom: 2
+        zoom: 1
     }
 
-  render() {
-    const position: LatLngTuple = [this.state.lat, this.state.lng];
-    return (
-        <Map bounds={[[-90, -180],[90, 180]]}
-          noWrap={true} dragging={false} center={position} zoom={this.state.zoom} minZoom={this.state.zoom} maxZoom={this.state.zoom}>
-            <TileLayer
+    position: LatLngTuple = [this.state.lat, this.state.lng];
+
+    map = <Map 
+            bounds={[[-90, -180],[90, 360]]} 
+            dragging={false} 
+            center={this.position} 
+            zoom={this.state.zoom}
+            minZoom={this.state.zoom} 
+            maxZoom={this.state.zoom}
+            onresize={this.updateMapSize}>
+        <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
+            noWrap={true} 
             />
-            <Marker position={position}>
-            <Popup>
-                A pretty CSS3 popup. <br/> Easily customizable.
-            </Popup>
-            </Marker>
+        <GeoJSON data={countries as any}></GeoJSON>
         </Map>
-    );
+
+    updateMapSize(evt: any) {
+        console.log(evt)
+    }
+
+    render() {
+        window.addEventListener('resize', this.updateMapSize);
+        return (this.map);
   }
 }
 
