3ba700216b1adb6c3f905266f69d52300618a504
weinrero <roland.weinreich@gmail.com>
2020-03-21 18:35:37 +0100

    COLORFUL!

--- src/components/MainMap.tsx
+++ src/components/MainMap.tsx
@@ -5,23 +5,44 @@ import 'leaflet-css';
 import './MainMap.css';
 import continents from '../data/continents.json';
 
-class MainMap extends React.Component<{}, { lat: number, lng: number, zoom: number  }> {
+class MainMap extends React.Component<{regions:{name: string, population: number, infectionRate: number, reproductionRate: number, happiness: number}[]}, { lat: number, lng: number, zoom: number  }> {
 
     constructor(props?: any, context?: any) {
         super(props, context);
         this.updateMapSize = this.updateMapSize.bind(this);
+        this.getColor = this.getColor.bind(this);
+        this.getStyle = this.getStyle.bind(this);
         this.state = {
-            lat: 0,
+            lat: 45,
             lng: 0,
             zoom: 2
         }
     }
 
     updateMapSize(evt:any) {
-        let zoomHeight = Math.log2(evt.newSize.y * 1.6 / 256)
+        let zoomHeight = Math.log2(evt.newSize.y * 2 / 256)
         let zoomWidth = Math.log2(evt.newSize.x * 1.1 / 256)
         this.setState({zoom: Math.floor(Math.min(zoomWidth*1.1, zoomHeight))});
         console.log(this.state.zoom)
+    }
+
+    getColor(region: any) {
+        console.log(region)
+        if (!region || !region.infectionRate) return `green`
+        let r=Math.round(region.infectionRate * 255)
+        let g=Math.round(100 - region.infectionRate * 100)
+        let b=Math.round(25 - region.infectionRate  * 25)
+        return `rgb(${r},${g},${b})`
+    }
+
+    getStyle(feature: any) {
+        return {
+            fillColor:  this.getColor(this.props.regions.filter((region) => region.name === feature.properties.CONTINENT)[0]),
+            weight: 1,
+            opacity: 1,
+            color: 'grey',
+            fillOpacity: 0.8
+        };
     }
 
     render() {
@@ -41,7 +62,7 @@ class MainMap extends React.Component<{}, { lat: number, lng: number, zoom: numb
             url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
             noWrap={true} 
             />
-        <GeoJSON data={continents as any}></GeoJSON>
+        <GeoJSON data={continents as any} style={this.getStyle}></GeoJSON>
         </Map>);
   }
 }
