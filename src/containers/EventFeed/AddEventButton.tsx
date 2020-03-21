dd1304311646c4e3402887cda2806864a1043aab
Nils Jannasch <nils.jannasch@tu-dortmund.de>
2020-03-21 22:29:29 +0100

    Dummy events added

--- src/containers/EventFeed/AddEventButton.tsx
+++ src/containers/EventFeed/AddEventButton.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 
 import { addEvent } from "../../actions";
 
+
 const AddEventButton = ({ dispatch }: { dispatch: any }) => {
-    return <button onClick={() => dispatch(addEvent("meintext", new Date()))}>Add Event</button>;
+    return <button onClick={() => dispatch(addEvent(new Date()))}>Add Event</button>;
 };
 
 export default connect()(AddEventButton)
