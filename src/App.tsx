d43cd8b8b1149eb756fa7709d4f93f9774d44b8b
Christof Weinreich <chr@weinreich-edv.de>
2020-03-22 09:44:45 +0100

    use sasss for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.sass';
 import AddInformMeasure from './containers/AddInformMeasure'
 import MainMap from './components/MainMap'
 import InformedStatus from './containers/InformedStatus';
