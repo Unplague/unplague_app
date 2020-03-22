b06e974efe3a314ca203eec4ee454e76fd2bd983
Christof Weinreich <chr@weinreich-edv.de>
2020-03-22 13:50:16 +0100

    Small stylle improvments (Events)

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ class App extends React.Component<MyProps, MyState> {
           <div className="right"></div>
         </div>
         <div className="content">
-          <div className="interactionboard">
+          <div className="interactionboard eventboard">
           <div className="innerboard"><RoundControl /></div>
           <div className="innerboard"><EventFeed /></div>
           </div>
