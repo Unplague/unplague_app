22af24f892c08662221e47d833485d2f48194ccf
Felix Seele <fseele@gmail.com>
2020-03-23 14:04:43 +0100

    updated help

--- src/components/HelpModal.tsx
+++ src/components/HelpModal.tsx
@@ -11,14 +11,14 @@ const HelpModal: FunctionComponent<Buttonrops> = () => {
         <a href="#" title="Close" className="modal-close">Close</a>
         <h1>HowTo Play</h1>
         <p>
-          The goal of the game is to keep the global infection rate <b>below 70%</b>.
+          The goal of the game is to keep the global infection rate <b>below 70%</b>. You can win by containing the virus so that there are no new infections.
         </p>
         <p>
           Click on a continent to see the individual statistics. 
-          To keep the number of infected people low and the happiness high, you can perform actions.
+          To reduce the speed by which the virus spreads, you can perform actions which affect the infection rate and the happiness of the population.
         </p>
         <p>
-          Caution: The level of happiness has also an influence on the costs (Toilet paper).
+          <b>But beware:</b> A low level of happiness will increase the cost of new actions. So use them carefully and find a balance to contain the virus without neglecting the people.
         </p>
         <hr/>
         <p>Hint:<br/>
